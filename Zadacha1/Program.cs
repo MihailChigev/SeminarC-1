552b6c374aac4862ff6314e12828dd40e46c281a
MikeChigev <mchigev@gmail.com>
2022-11-21 21:29:56 +0300

    квадрат

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int  number = int.Parse(Console.ReadLine());
+int square = number*number;
+Console.WriteLine($"квадрат числа {number} равен {square}");
+
