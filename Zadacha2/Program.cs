7dc15ad594fb5ef8241d3f681914fee48061c2a4
MikeChigev <mchigev@gmail.com>
2022-11-22 12:31:44 +0300

    вывод последнего числа

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу которая принимат трехзначное число а на выходе показывает последнюю цифру этого числа 
+Console.Clear();
+Console.WriteLine("Введите число ");
+int n1 = int.Parse(Console.ReadLine());
+int n2 = n1%10;
+Console.WriteLine(n2);
